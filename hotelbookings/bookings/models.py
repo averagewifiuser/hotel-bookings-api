17e8eb09c353b12dce31feb5c0121592e87b3f86
@joseph3241 <joseph3241@gmail.com>
2022-03-30 14:41:19 +0100

    added the booking

--- hotelbookings/bookings/models.py
+++ hotelbookings/bookings/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.utils import timezone
+
+def one_day_hence():
+    return timezone.now() + timezone.timedelta(days=1)
 
 class Room(models.Model):
     number = models.CharField(max_length=30, blank=False, unique=True)
@@ -9,3 +13,18 @@ class Room(models.Model):
 
 
 
+class Booking(models.Model):
+    '''
+    room_id fk default null
+    booked_by fk default null
+    starts default timezone.now
+    ends timezone.now + 1 day
+    confirmed default false
+
+    '''
+    room = models.ForeignKey(Room, default=None)
+    #booked_by = models.ForeignKey('customers.Customer', default=None)
+    start = models.DateTimeField(default=timezone.now)
+    end = models.DateTimeField(default=one_day_hence)
+    confirmed = models.BooleanField(default=False)
+    
